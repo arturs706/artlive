de84e1539b3447397d4ca5ecb98866af8ac506f3
Arturs <arturs@Arturss-Air.broadband>
2022-05-01 23:45:38 +0100

    first-commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import { SessionProvider } from "next-auth/react"
-import Layout from '../components/Layout'
+import Layout from '../Components/Layout'
 
 
 export default function App({
